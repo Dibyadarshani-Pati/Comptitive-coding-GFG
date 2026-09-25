6510b564c34abf7b4808ee922103468def7389dc
Dibyadarshani Pati <dibyadarshanipati29@gmail.com>
2026-09-25 19:08:49 +0530

    Add GFG solution: Anagram | Practice

---
+++ GeeksForGeeks/Arrays/anagram-practice.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        int[] freq = new int[26]; 
+
+        for (char c : s1.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : s2.toCharArray()) {
+            freq[c - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) return false;
+        }
+        return true;
+    }
+}
