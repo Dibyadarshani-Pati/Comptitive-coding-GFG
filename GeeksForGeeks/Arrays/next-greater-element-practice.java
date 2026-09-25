08dfd591f09a1646757db8aa28d62af6423a956d
Dibyadarshani Pati <dibyadarshanipati29@gmail.com>
2026-09-25 18:37:58 +0530

    Add GFG solution: Next Greater Element | Practice

---
+++ GeeksForGeeks/Arrays/next-greater-element-practice.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>(n);
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result.add(0, -1);  
+            } else {
+                result.add(0, stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+    }
+}
